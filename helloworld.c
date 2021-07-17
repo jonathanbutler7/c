b915a298eae799bd02781e3765d7d20365888974
Jonathan Butler <jonathan.butler@getweave.com>
2021-07-17 16:38:44 -0700

    code compiles and runs

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main()
 {
-  printf("Hello world!");
+  // cmd + shift + b will recompile after making changes
+  printf("Hello world!!\n");
   return 0;
 }
+// ctrl + opt + n will compile and give terminal with one command using Code Runner
