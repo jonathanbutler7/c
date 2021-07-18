974dc30418bbbbe0c89312cf442b9a492988415b
Jonathan Butler <jonathan.butler@getweave.com>
2021-07-17 17:00:46 -0700

    adds some notes

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+
 int main()
 {
   // cmd + shift + b will recompile after making changes
+  /* 
+  multi line comments 
+  look like this
+  */
   printf("Hello world!!\n");
+  printf("Goodbye world!!\n");
   return 0;
 }
 // ctrl + opt + n will compile and give terminal with one command using Code Runner
