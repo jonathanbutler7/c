81278d2fb4d9edff814f66a8967ff50d209abd67
Jonathan Butler <jonathan.butler@getweave.com>
2022-01-08 21:34:58 -0800

    notes added Sat Jan  8 21:34:58 PST 2022

--- helloworld.c
+++ helloworld.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 // stdio means standard input/output
 // #include "stdio.h" would mean it will look first in the current directory for a user defined lib
-int another()
-{
-  printf("Here is another one");
-  return 0;
-}
+
 int main()
 {
   // cmd + shift + b will recompile after making changes
@@ -13,10 +9,14 @@ int main()
   multi line comments 
   look like this
   */
-  printf("Hello world!\n");
-  printf("Goodbye world!\n");
-  another();
-  another();
+  // char str[100];
+  int i;
+  printf("Enter a value:");
+
+  scanf("%d", &i);
+
+  printf("\nYou  entered: %d\n", i);
+
   return 0;
 }
 // ctrl + opt + n will compile and give terminal with one command using Code Runner
