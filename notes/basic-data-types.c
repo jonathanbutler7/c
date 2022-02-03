75e2df90dea4089c7649c540f0a7143d5db8539d
Jonathan Butler <jonathan.butler@getweave.com>
2022-02-02 20:03:00 -0800

    notes added Wed Feb  2 20:03:00 PST 2022

--- notes/basic-data-types.c
+++ notes/basic-data-types.c
@@ -4,17 +4,6 @@ Purpose: This program prints my name to the screen
 Date:
 */
 
-// int x;
-// int x, y, x;
-
-// x = 112;
-
-// int x = 21;
-// int y = 32, z = 14;
-// int x, z = 94 /* valid but poor form */
-
-// in the line above, only z is initialized.
-
 #include <stdio.h>
 #include <stdbool.h> // #include <stbool.h> to have standard true/false keywords as a data type
 
@@ -30,5 +19,17 @@ int main()
   _Bool boolVariable = 1;
 
   bool something = false;
+
   return 0;
 }
+
+/*
+Three adjective keywords to modify basic integer type( can also be used by themselves)
+- short
+- long
+- unsigned
+*/
+
+long int numberOfPoints = 13245300L;
+long double US_deficit_2017 = 1.234e+7L;
+
---
+++ notes/overview.c
@@ -0,0 +1,10 @@
+int x;
+int x, y, x;
+
+x = 112;
+
+int x = 21;
+int y = 32, z = 14;
+int x, z = 94 // <- valid but poor form
+
+       // in the line above, only z is initialized.
