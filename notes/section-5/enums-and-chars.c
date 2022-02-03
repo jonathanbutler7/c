9bb7afd1e9a48ef9d1a4be85bf02406805a71cc4
Jonathan Butler <jonathan.butler@getweave.com>
2022-02-02 20:15:39 -0800

    notes added Wed Feb  2 20:15:39 PST 2022

--- notes/section-5/enums-and-chars.c
+++ notes/section-5/enums-and-chars.c
@@ -7,7 +7,7 @@ enum primaryColor
 
 enum primaryColor myColor, gregColor;
 myColor = red;   // this is good
-myColor = green; // this is bad
+// myColor = green; // this is bad
 
 enum direction
 {
@@ -17,3 +17,38 @@ enum direction
   right      // = 11, not 3
 };
 
+// char data type is a single character in single quotes
+
+char broiled;
+broiled = 'T'; // good
+// broiled = T;   // not good
+broiled = "T"; // not good because double quotes means string (different from char)
+
+char x = '\n'; // if you print this, it just prints a new line
+
+// \a, \b, \f, \n, \r are all escape characters
+
+#include <stdio.h>
+
+int main()
+{
+  enum gender
+  {
+    male,
+    female
+  };
+
+  enum gender myGender;
+
+  myGender = male;
+
+  // myGender = "44" // wrong because you can on ly assign male or female
+
+  enum gender anotherGender = female;
+
+  _Bool isMale = (myGender == anotherGender);
+  
+  printf(anotherGender);
+
+  return 0;
+};
