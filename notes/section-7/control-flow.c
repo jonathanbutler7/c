6f62c07d7eadcfea2bd88e506e47acb2cea3adac
Jonathan Butler <jonathan.butler@getweave.com>
2022-02-14 21:44:49 -0800

    notes added Mon Feb 14 21:44:49 PST 2022

---
+++ notes/section-7/control-flow.c
@@ -0,0 +1,36 @@
+/**
+ * @file control-flow.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-02-14
+ *
+ * @copyright Copyright (c) 2022
+ *
+ * if statements
+ *
+ *
+ *
+ */
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+  int number, sign;
+  printf("Type a number:\n");
+  scanf("%i", &number);
+
+  // if (number < 0)
+  // {
+  //   sign = -1;
+  // }
+  // else if (number == 0)
+  // {
+  //   sign = 0;
+  // }
+  // else
+  //   sign = 1;
+  number < 0 ? sign = -1 : number == 0 ? sign = 0 : 1;
+  printf("Sign = %i\n", sign);
+  return 0;
+}
