8215a4d9f71be6daea7946a7fbc9c13772d62f5e
Jonathan Butler <jonathan.butler@getweave.com>
2022-02-14 20:39:30 -0800

    notes added Mon Feb 14 20:39:30 PST 2022

--- notes/section-5/minutes-years-days.c
+++ notes/section-5/minutes-years-days.c
@@ -3,5 +3,16 @@
 int main()
 {
   
+  long int minutesEntered;
+  printf("Enter minutes: ");
+  scanf("%ld", &minutesEntered);
+  printf("\nMinutes entered: %ld\n", minutesEntered);
+
+  float hours = minutesEntered / 60.0;
+  float days = hours / 24.0;
+  float years = days / 365.0;
+  printf("\nMinutes is approximately %d days", (int)days);
+  printf("\nYears entered: %lf\n", years);
+
   return 0;
 }
