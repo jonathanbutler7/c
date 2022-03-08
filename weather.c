ec017ab9c0a92274d32e7ddb0b84f57dc06550ea
Jonathan Butler <jonathan.butler@getweave.com>
2022-03-08 09:52:36 -0800

    notes added Tue Mar  8 09:52:36 PST 2022

--- weather.c
+++ weather.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-  float yearlyRainfall[YEARS][MONTHS] =
+  float rain[YEARS][MONTHS] =
       {
           {4.3, 4.3, 4.3, 3.0, 2.0, 1.2, 0.2, 0.2, 0.4, 2.4, 3.5, 6.6},
           {8.5, 8.2, 1.2, 1.6, 2.4, 0.0, 5.2, 0.9, 0.3, 0.9, 1.4, 7.3},
@@ -13,16 +13,25 @@ int main()
           {7.6, 5.6, 3.8, 2.8, 3.8, 0.2, 0.0, 0.0, 0.0, 1.3, 2.6, 5.2}};
 
   float total, subtotal;
-
-  for (int year = 0, total = 0; year < YEARS; ++year)
+  int year, month;
+  for (year = 0, total = 0; year < YEARS; year++)
   {
-    printf("\n\n%d total: %d\n", 2010 + year, total);
-    total = 0;
-    for (int month = 0; month < MONTHS; ++month)
+    for (month = 0, subtotal = 0; month < MONTHS; month++)
     {
-      total += yearlyRainfall[year][month];
-      printf("%d ", total);
+      subtotal += rain[year][month];
     }
+    printf("%5d \t%15.1f\n", 2010 + year, subtotal);
+    total += subtotal;
   }
+  // for (int year = 0, total = 0; year < YEARS; ++year)
+  // {
+  //   printf("\n\n%d total: %d\n", 2010 + year, total);
+  //   total = 0;
+  //   for (int month = 0; month < MONTHS; ++month)
+  //   {
+  //     total += yearlyRainfall[year][month];
+  //     printf("%d ", total);
+  //   }
+  // }
   return 0;
 }
